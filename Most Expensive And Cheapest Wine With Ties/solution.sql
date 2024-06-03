88cc63949b1e7fd801934531d18f9f5b491dca6d
Komal Diwe <ksdiwe2615@gmail.com>
2024-06-03 15:36:08 +0530

    SQL solution

---
+++ Most Expensive And Cheapest Wine With Ties/solution.sql
@@ -0,0 +1,28 @@
+-- Combine region_1 and region_2 into a single column `region`
+WITH combine_region AS(
+    SELECT region_1 AS region,
+        variety, price
+    FROM winemag_pd
+    WHERE price IS NOT NULL and region_1 IS NOT NULL
+    UNION ALL
+    SELECT region_2 AS region,
+        variety, price
+    FROM winemag_pd
+    WHERE price IS NOT NULL and region_2 IS NOT NULL),
+    
+-- Rank wines within each region by price
+ranked_cte AS(
+    SELECT region, variety,
+        DENSE_RANK() OVER(PARTITION BY region ORDER BY price DESC) as expensive_rank,
+        DENSE_RANK() OVER(PARTITION BY region ORDER BY price) as cheapest_rank
+    FROM combine_region)
+    
+-- Self-join to combine the most expensive and cheapest varieties for each region
+SELECT
+    DISTINCT t1.region,
+    t1.variety AS expensive_variety,
+    t2.variety AS cheapest_variety
+FROM ranked_cte t1 
+JOIN ranked_cte t2
+ON t1.region = t2.region AND t1.expensive_rank = t2.cheapest_rank
+WHERE t1.expensive_rank = 1 and t2.cheapest_rank = 1;
